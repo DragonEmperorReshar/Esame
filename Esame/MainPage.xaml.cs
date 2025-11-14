63f4b6557897bb0cfdb23cd5f29e6de5c441ecc0
Alessio Casti <43cannotfun@gmail.com>
2025-11-14 17:30:39 +0100

    Commit 2

--- Esame/MainPage.xaml.cs
+++ Esame/MainPage.xaml.cs
@@ -1,13 +1,20 @@
-﻿namespace Esame
+﻿using AudioToolbox;
+using System.Text.Json.Nodes;
+
+namespace Esame
 {
     public partial class MainPage : ContentPage
     {
         RestService restService;
         List<Product> Items;
 
+
+
         public MainPage()
         {
             InitializeComponent();
+            var service = new RestService(); 
+                BindingContext = this;
         }
 
         private async void OnCounterClicked(object sender, EventArgs e)
@@ -16,9 +23,28 @@
            var products = await service.GetProductsAsync();
         }
 
-        private async void OnSelectionChanged(object sender, EventArgs e)
+        protected async override void OnNavigatedTo(NavigationEventArgs args)
         {
-            await Shell = (GoToAsync)
+            base.OnNavigatedTo(args);
+            var products = await ServiceCollection.GetProductsAsync();
+            foreach (var p in products)
+            {
+                Items.Add(p);
+            }
+
+        }
+
+        private async void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.CurrentSelection.FirstOrDefault() is not Product item)
+                return;
+
+            await Shell.Current.GoToAsync(nameof(Product), true, new Dictionary<string, object>
+            {
+                ["Item"] = item
+            });
+
+
             //var service = new RestService();
             //var prducts = await service.GetProductsAsync(product);
         }
